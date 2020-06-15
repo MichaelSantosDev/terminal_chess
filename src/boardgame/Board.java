a06bea6970899f44642ded49043f1f6bbe95f52a
neoMichael <40837668+neoMichael@users.noreply.github.com>
2020-06-15 15:55:55 +0100

    BoardException implementarion

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -22,22 +22,46 @@ public class Board {
 	}
 	
 	
-	public Piece piece(int row, int column) {
+	public Piece piece(int row, int column) {		
+		if(!validPosition(row, column)) {
+			throw new BoardException("This positon is not valid");
+		}		
 		return pieces[row][column];		
 	}
 	
 	
-	public Piece piece(Position position) {
+	public Piece piece(Position position) {		
+		if(!validPosition(position)) {
+			throw new BoardException("This positon is not valid");
+		}		
 		return pieces[position.getRow()][position.getColumn()];		
 	}
 	
 	
 	public void placePiece(Piece piece, Position position) {
-		
+		if(isThereAPiece(position)) {
+			throw new BoardException("Already has a piece in position " + position);
+		}		
 		pieces[position.getRow()][position.getColumn()] = piece;
-		piece.position = position;
-		
+		piece.position = position;		
+	}
+	
+	
+public boolean validPosition(int row, int column) {	
+	return row >= 0 && row < ROW && column >= 0 && column < COLUMN;		
 	}
 
+	
+	public boolean validPosition(Position position) {		
+		return validPosition(position.getRow(), position.getColumn());		
+	}
+	
+	
+	public boolean isThereAPiece(Position position) {
+		if(!validPosition(position)) {
+			throw new BoardException("This position is not valid.");
+		}		
+		return piece(position) != null;	
+	}
 	
 }
